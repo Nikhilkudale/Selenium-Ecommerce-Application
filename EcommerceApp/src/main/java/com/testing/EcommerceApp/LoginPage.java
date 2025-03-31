33c59df9f40e83979839c65300c1c20f3bbfb288
NI20529338 <NI20529338@APL-5CD4070B3G>
2025-03-31 15:19:53 +0530

    Commit Added Special Conditions

--- EcommerceApp/src/main/java/com/testing/EcommerceApp/LoginPage.java
+++ EcommerceApp/src/main/java/com/testing/EcommerceApp/LoginPage.java
@@ -2,6 +2,7 @@ package com.testing.EcommerceApp;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage {
     WebDriver driver;
@@ -11,8 +12,21 @@ public class LoginPage {
     }
 
     public void login(String username, String password) {
-        driver.findElement(By.id("user-name")).sendKeys(username);
-        driver.findElement(By.id("password")).sendKeys(password);
+    	slowType(driver.findElement(By.id("user-name")), username); // Corrected call to slowType
+        slowType(driver.findElement(By.id("password")), password); // Added slowType for password
         driver.findElement(By.id("login-button")).click();
     }
+
+
+
+private void slowType(WebElement element, String text) {
+    for (char c : text.toCharArray()) {
+        element.sendKeys(String.valueOf(c));
+        try {
+            Thread.sleep(200); // 200 milliseconds delay between each keystroke
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
 }
